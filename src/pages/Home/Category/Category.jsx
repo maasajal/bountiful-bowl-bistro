e9ad7e9e95596bdde881444a703159bab97d493c
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-18 02:26:40 +0300

    Category carousel using Sweper slider

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,70 @@
+// core version + navigation, pagination modules:
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <div className="py-14">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper text-white pb-10"
+      >
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slide1} alt="Category slide" />
+            <h3 className="uppercase text-center font-Cinzel text-2xl absolute bottom-6 left-16">
+              Salads
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slide2} alt="Category slide" />
+            <h3 className="uppercase text-center font-Cinzel text-2xl absolute bottom-6 left-16">
+              Pizzas
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slide3} alt="Category slide" />
+            <h3 className="uppercase text-center font-Cinzel text-2xl absolute bottom-6 left-16">
+              Soups
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slide4} alt="Category slide" />
+            <h3 className="uppercase text-center font-Cinzel text-2xl absolute bottom-6 left-16">
+              Desserts
+            </h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slide5} alt="Category slide" />
+            <h3 className="uppercase text-center font-Cinzel text-2xl absolute bottom-6 left-16">
+              Salads
+            </h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+export default Category;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 
 const Home = () => {
   return (
@@ -6,6 +7,7 @@ const Home = () => {
       <Banner />
       <div className="max-w-5xl mx-auto px-2 md:px-4 py-20">
         <h1 className="text-5xl font-Cinzel font-bold text-black">Home</h1>
+        <Category />
       </div>
     </>
   );
