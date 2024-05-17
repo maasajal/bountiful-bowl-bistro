dcec760a661d72fc0a79388b13947c89dddcc6c8
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-18 01:13:32 +0300

    NavBar added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
   return (
-    <>
+    <div className="font-Inter">
+      <NavBar />
       <div>
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 export default Main;
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,7 +1,106 @@
+import { Link, NavLink } from "react-router-dom";
+import profilePic from "../../../assets/logo.png";
+import logo from "../../../assets/bbb-logo.png";
+import "./Navbar.css";
 const NavBar = () => {
+  const navLinks = (
+    <>
+      <li key="home" className="hover:text-[#EEFF25]">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li key="contact-us" className="hover:text-[#EEFF25]">
+        <NavLink to="/contact-us">Contact Us</NavLink>
+      </li>
+      <li key="dashboard" className="hover:text-[#EEFF25]">
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      </li>
+      <li key="our-menu" className="hover:text-[#EEFF25]">
+        <NavLink to="/our-menu">Our Menu</NavLink>
+      </li>
+      <li key="our-shop" className="hover:text-[#EEFF25]">
+        <NavLink to="/our-shop">Our Shop</NavLink>
+      </li>
+    </>
+  );
   return (
     <>
-      <h2>NavBar Component</h2>
+      <header className="navbar py-4 md:px-14 bg-[#15151580] text-white font-extrabold">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-[#EEFF25]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#15151580] rounded-box w-52 uppercase"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <a className="btn btn-ghost h-full w-40">
+            <img src={logo} alt="company logo" />
+          </a>
+        </div>
+        <div className="navbar items-center justify-end gap-2 md:gap-4">
+          <ul className="menu menu-horizontal px-1 hidden lg:flex uppercase">
+            {navLinks}
+          </ul>
+          <div className="w-10 rounded-full">
+            <img src={profilePic} alt="Cart" />
+          </div>
+          <Link
+            to="/login"
+            className="btn btn-outline px-5 text-white font-extrabold"
+          >
+            Login
+          </Link>
+          <Link
+            to="/"
+            className="btn btn-outline px-5 text-white font-extrabold"
+          >
+            Sign Out
+          </Link>
+          <div className="dropdown dropdown-end mr-1 tooltip tooltip-bottom tooltip-primary z-10">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img src={profilePic} alt="Logged user photo" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black rounded-box w-52"
+            >
+              <li>
+                <NavLink to="/user-profile" className="py-3 hover:text-white">
+                  User Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/update-profile" className="py-3 hover:text-white">
+                  Update Profile
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
