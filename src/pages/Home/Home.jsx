76accf6d42e878f47494043d16115ded303de15d
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-18 15:04:42 +0300

    Call us sectio added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,12 @@ const Home = () => {
       <div className="max-w-6xl mx-auto px-2 md:px-4 py-16">
         <Category />
         <PopularMenu />
+        <section className="bg-[#151515] text-white py-20 text-center font-Raleway text-3xl mb-20">
+          <p>
+            Call Us:
+            <a href="callto:+880192345678910"> +88 0192345678910</a>
+          </p>
+        </section>
         <ChefRecommends />
       </div>
     </>
